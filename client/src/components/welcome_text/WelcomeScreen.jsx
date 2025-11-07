ca5ec9cd25083331d806d260deb15d061fe2258a
Ayush Katiyar <ayush24katiyar@gmail.com>
2025-11-07 11:19:01 +0530

    minor change

--- client/src/components/welcome_text/WelcomeScreen.jsx
+++ client/src/components/welcome_text/WelcomeScreen.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 function WelcomeScreen() {
   return (
-    <div className="flex flex-col items-center justify-center h-full gap-2 px-4">
-      <h1 className="text-6xl font-extralight text-white">
-        Pulse<span className="text-blue-500 font-light">AI</span>
+    <div className="flex flex-col items-center justify-center h-full gap-2 px-4 ">
+      <h1 className="text-6xl font-extralight dark:text-white text-white ">
+        <span className='text-gray-900 dark:text-white  '>
+            Pulse
+            </span>
+            
+            <span className="text-blue-500 font-light  ">AI</span>
       </h1>
       <p className="text-gray-400 text-base">
         What's bothering you today ?
